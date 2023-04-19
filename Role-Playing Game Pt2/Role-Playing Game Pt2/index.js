702daff85f6f566bf971b21398eced8a36869a43
Karl Chavez <105255669+KarlChavez@users.noreply.github.com>
2023-04-19 16:53:44 -0400

    Add files via upload

---
+++ Role-Playing Game Pt2/Role-Playing Game Pt2/index.js
@@ -0,0 +1,11 @@
+import characterData from '/data.js'
+import Character from '/Character.js'
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+}
+
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+render()
